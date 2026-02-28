bd4022ca38f5b1a2b73102ccd37d8eba1f1d7316
asothii <cheetodeveloper@gmail.com>
2026-02-28 14:43:07 +0000

    Nix fixes

--- nix/module.nix
+++ nix/module.nix
@@ -464,6 +464,7 @@ let
 
   yeetmouse = pkgs.yeetmouse.override {
     kernel = config.boot.kernelPackages.kernel;
+    kernelModuleMakeFlags = config.boot.kernelPackages.kernelModuleMakeFlags;
   };
 in {
   options.hardware.yeetmouse = {
@@ -591,7 +592,6 @@ in {
     nixpkgs.overlays = [ yeetmouseOverlay ];
 
     boot.extraModulePackages = [ yeetmouse ];
-    environment.systemPackages = [ yeetmouse ];
     services.udev = {
       extraRules = let
         echo = "${pkgs.coreutils}/bin/echo";
